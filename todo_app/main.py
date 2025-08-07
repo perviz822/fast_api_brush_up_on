98fe5057844340f4098028a4cbef05c51a3f2db9
Parviz <perviz822@gmail.com>
2025-08-07 20:11:34 +0200

    routers arranged for scalability

--- todo_app/main.py
+++ todo_app/main.py
@@ -2,73 +2,13 @@
 from fastapi import FastAPI, Depends, status
 import models
 from database import engine
-from database import SessionLocal
-from typing import Annotated
-from sqlalchemy.orm import Session
-from models import Todos
-from  fastapi.exceptions import HTTPException
-from pydantic import BaseModel, Field
-app= FastAPI()
-from routers import auth
+
+from routers import auth,todos
 
 models.Base.metadata.create_all(bind=engine)
 
+
+app= FastAPI()
+
 app.include_router(auth.router)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()    
-
-
-db_dependency = Annotated[Session, Depends(get_db) ]
-
-
-class TodoRequest(BaseModel):
-     title: str = Field(min_length=3,max_length=100)
-     description: str= Field(min_length=3,max_length=100)
-     priority: int =  Field(gt=0,lt=6)
-     complete: bool
-
-
-
-
-
-@app.get("/")
-async def read_all( db:db_dependency ):
-      return  db.query(Todos).all()
-                                                      
-
-
-@app.get("/todo/{todo_id}")
-async def read_todo(db:db_dependency,todo_id:int):
-     todo_model = db.query(Todos).filter(Todos.id==todo_id).first()
-     if todo_model is not None:
-          return todo_model
-     raise HTTPException(status_code=404,detail="Todo not found")
-
-
-@app.post("/todo")
-async def create_todo(db:db_dependency,todo_request:TodoRequest):
-     todo_model = Todos(**todo_request.model_dump())
-     db.add(todo_model)
-     db.commit()
-
-
-
-
-@app.put("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo(db:db_dependency,
-                      todo_id:int,
-                      todo_request:TodoRequest):
-     todo_model = db.query(Todos).filter(Todos.id ==todo_id).first()
-     if todo_model is None:
-          raise HTTPException(status_code=404,detail="todo not found")
-     todo_model.title = todo_request.title
-     todo_model.priority = todo_request.priority
-     todo_model.description = todo_request.description
-     todo_model.complete = todo_request.complete
-     db.commit()
-     db.refresh(todo_model)
+app.include_router(todos.router)

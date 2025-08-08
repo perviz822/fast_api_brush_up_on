75aa84d01e3297931748e89a1429e2bc521e0370
Parviz <perviz822@gmail.com>
2025-08-08 13:22:20 +0200

    JWT implemented

--- todo_app/routers/auth.py
+++ todo_app/routers/auth.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from fastapi import FastAPI
 from fastapi import APIRouter
 from pydantic import BaseModel
@@ -8,7 +9,12 @@ from typing import Annotated
 from database import SessionLocal
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 router = APIRouter()
+
+SECRET_KEY ='d79e820b57144f5468a1b63a1baff6241b62f950321ef3b0b0042206a502a33a'
+ALGORITHM ='HS256'
+
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class CreateUserRequest(BaseModel):
@@ -29,14 +35,19 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db) ]
 
-    
 def authenticate_user(username:str,password:str,db):
     user=db.query(Users).filter(Users.username==username).first()
     if not user:
         return    
     if  not bcrypt_context.verify(password,user.hashed_password):
         return False
-    return True 
+    return user
+
+def create_access_token(username:str, user_id:int, expires_delta:timedelta):
+    encode = {'sub':username,'id':user_id}
+    expires =datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp':expires})  #type: ignore
+    return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
 
 
 
@@ -62,13 +73,13 @@ async def create_user(
 @router.post("/token")
 async def  login_for_access_token(form_data:Annotated[OAuth2PasswordRequestForm,Depends()],
                                   db:db_dependency):
-    
 
     user = authenticate_user(form_data.username,form_data.password,db)
 
     if not user:
         return "Failed authentication"
-    
-    return 'Authentication succesfull'
+    token = create_access_token(user.username, user.id, timedelta(minutes=20)) #type:ignore
+
+    return token
 
 
